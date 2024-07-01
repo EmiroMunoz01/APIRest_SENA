d0994a4567e631a4d989188fa3fe2d9c08eb8339
EmiroMunoz01 <emiro_2001@icloud.com>
2024-07-01 17:43:54 -0500

    Initial commit

---
+++ src/main/java/com/example/apirest_sena/ApiRestSenaApplication.java
@@ -0,0 +1,13 @@
+package com.example.apirest_sena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apirest_sena/ApiRestSenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirest_sena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
