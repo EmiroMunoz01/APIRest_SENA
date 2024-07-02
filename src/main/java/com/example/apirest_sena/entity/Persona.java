c95bd7d29db88f931aa0737b43231d4c53cedd85
EmiroMunoz01 <emiro_2001@icloud.com>
2024-07-01 19:27:54 -0500

    Create the class Persona, in this class I create the params necesary for the function in the bd

--- src/main/java/com/example/apirest_sena/entity/Persona.java
+++ src/main/java/com/example/apirest_sena/entity/Persona.java
@@ -1,12 +1,17 @@
 package com.example.apirest_sena.entity;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
-
+@Entity
+@Table(name = "tabla_personas")
 public class Persona {
 
-
+    @Id
+    private long id;
 
 }
