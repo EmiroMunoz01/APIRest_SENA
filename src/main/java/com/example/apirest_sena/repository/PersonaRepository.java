8a9fa269e18c2778655ac6e1ff8be71ad8d284d3
EmiroMunoz01 <emiro_2001@icloud.com>
2024-07-03 20:01:28 -0500

    Creacion del repositorio, de aqui extenderan todos los metodos que se requieran

--- src/main/java/com/example/apirest_sena/repository/PersonaRepository.java
+++ src/main/java/com/example/apirest_sena/repository/PersonaRepository.java
@@ -1,6 +1,6 @@
 package com.example.apirest_sena.repository;
 
-
+//
 import com.example.apirest_sena.entity.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
