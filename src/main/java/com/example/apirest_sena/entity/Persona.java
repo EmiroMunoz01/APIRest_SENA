7584afa6a66581b81ad2eb556f707eee816603de
EmiroMunoz01 <emiro_2001@icloud.com>
2024-07-03 20:00:22 -0500

    Creacion del repositorio, de aqui extenderan todos los metodos que se requieran

--- src/main/java/com/example/apirest_sena/entity/Persona.java
+++ src/main/java/com/example/apirest_sena/entity/Persona.java
@@ -1,17 +1,27 @@
 package com.example.apirest_sena.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "tabla_personas")
 public class Persona {
-//comentario de verificacion
-    @Id
-    private long id;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+
+    private long personaId;
+
+    private String nombre;
+
+    private String apellido;
+
+
+    private String edad;
+
+    @Column(name = "email_direccion", unique = true, nullable = false)
+    private String email;
 }
---
+++ src/main/java/com/example/apirest_sena/repository/PersonaRepository.java
@@ -0,0 +1,11 @@
+package com.example.apirest_sena.repository;
+
+
+import com.example.apirest_sena.entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonaRepository extends JpaRepository<Persona, Long> {
+
+}
